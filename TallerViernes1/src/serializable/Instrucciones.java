1eea141d3977d1a2e59b2041b280654e11dd23d9
1107513806 <1107513806@sc309e20.academico.icesi.edu.co>
2016-09-30 08:04:01 -0500

    con clase serializable

---
+++ TallerViernes1/src/serializable/Instrucciones.java
@@ -0,0 +1,27 @@
+package serializable;
+
+import java.io.Serializable;
+
+public class Instrucciones implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public float x;
+	public float y;
+	public float tamano;
+	public float r,g,b;
+	
+	
+	public Instrucciones(float x, float y, float tam, float r, float g, float b){
+		this.x=x;
+		this.y=y;
+		this.tamano=tam;
+		this.r=r;
+		this.g=g;
+		this.b=b;
+	}
+
+}
