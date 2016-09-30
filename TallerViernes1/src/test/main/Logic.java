5ccffd629c9f9cdc63fa0a71c53978053d01fc38
1107513806 <1107513806@sc309e20.academico.icesi.edu.co>
2016-09-30 08:12:11 -0500

    con comunicacion

--- TallerViernes1/src/test/main/Logic.java
+++ TallerViernes1/src/test/main/Logic.java
@@ -2,20 +2,26 @@ package test.main;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
 import processing.core.PApplet;
+import test.control.Comunicacion;
 import test.control.ControlBaseDatos;
 import test.modelo.Bolita;
 
-public class Logic {
+public class Logic implements Observer{
 	private PApplet app;
 	private ArrayList<Bolita> bolitas;
 	private ControlBaseDatos controlBD;
+	private Comunicacion com;
 	
 	public Logic(PApplet app){
 		this.app= app;
 		this.controlBD= new ControlBaseDatos("data"+File.separator+"bolitas.xml");
 		this.bolitas = controlBD.leerBolitas();
+		com= new Comunicacion();
+		com.addObserver(this);
 	}
 
 	public void pintar() {
@@ -31,6 +37,12 @@ public class Logic {
 		Bolita nueva = new Bolita(app.mouseX, app.mouseY);
 		bolitas.add(nueva);
 		controlBD.agregarBolita(nueva);
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		System.out.println("entro-update");
 		
 	}
 	
